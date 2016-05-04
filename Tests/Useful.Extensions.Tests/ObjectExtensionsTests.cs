90c27813506eb8a086c7ca6a386cf8e1281f3c9b
Tazmainiandevil <mark@twistersdomain.co.uk>
2016-05-04 11:34:34 +0100

    Improved getting values from Anonymous types

--- Tests/Useful.Extensions.Tests/ObjectExtensionsTests.cs
+++ Tests/Useful.Extensions.Tests/ObjectExtensionsTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Dynamic;
 using FluentAssertions;
 using Xunit;
 
@@ -7,6 +8,31 @@ namespace Useful.Extensions.Tests
     public class ObjectExtensionsTests
     {
         #region Get Value From Anon Tests
+
+        [Fact]
+        public void test_get_value_from_anonymous_type_with_null_object_throws_exception()
+        {
+            // Arrange
+            ExpandoObject anonObject = null;
+
+            // Act
+            // Assert
+            Assert.Throws<ArgumentNullException>(() => ObjectExtensions.GetValueFromAnonymousType<string>(anonObject, "Name"));
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("    ")]
+        [InlineData(null)]
+        public void test_get_value_from_anonymous_type_with_null_or_empty_property_throws_exception(string  property)
+        {
+            // Arrange
+            var anonObject = new { Name = "Bob" };
+
+            // Act
+            // Assert            
+            Assert.Throws<ArgumentNullException>(() => ObjectExtensions.GetValueFromAnonymousType<string>(anonObject, property));
+        }
 
         [Fact]
         public void test_get_value_from_anonymous_type_for_a_string_returns_the_expected_value()
@@ -34,7 +60,135 @@ namespace Useful.Extensions.Tests
             result.Should().Be(3);            
         }
 
+
+        [Fact]
+        public void test_get_value_from_anonymous_type_when_the_property_does_not_exist_throws_an_exception()
+        {
+            // Arrange
+            var anonObject = new { Number = 3 };
+
+            // Act                        
+            // Assert
+            Assert.Throws<ArgumentException>(() => ObjectExtensions.GetValueFromAnonymousType<int>(anonObject, "Something"));
+        }
+
+
+        [Fact]
+        public void test_get_value_from_anonymous_type_when_the_property_exists_but_the_type_is_different_throws_an_exception()
+        {
+            // Arrange
+            var anonObject = new { Number = 3 };
+
+            // Act                        
+            // Assert
+            Assert.Throws<InvalidCastException>(() => ObjectExtensions.GetValueFromAnonymousType<string>(anonObject, "Number"));
+        }
+
+        [Theory]
+        [InlineData(typeof(int))]
+        [InlineData(typeof(string))]
+        [InlineData(typeof(double))]
+        [InlineData(typeof(long))]
+        [InlineData(typeof(DateTime))]
+        public void test_get_value_from_anonymous_type_or_default_when_the_property_does_not_exist_returns_the_default<T>(T thetype)
+        {
+            // Arrange
+            var anonObject = new { Number = 3 };
+
+            // Act
+            var result = ObjectExtensions.GetValueFromAnonymousTypeOrDefault<T>(anonObject, "Something");
+
+            // Assert
+            result.Should().Be(default(T));
+        }
+
+        [Fact]
+        public void test_get_value_from_anonymous_type_or_default_when_the_property_exists_but_the_type_is_different_throws_an_exception()
+        {
+            // Arrange
+            var anonObject = new { Number = 3 };
+
+            // Act            
+            // Assert
+            Assert.Throws<InvalidCastException>(() => ObjectExtensions.GetValueFromAnonymousTypeOrDefault<string>(anonObject, "Number"));
+        }
+
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("    ")]
+        [InlineData(null)]
+        public void test_get_value_from_anonymous_type_or_default_with_null_or_empty_property_returns_the_default_value(string property)
+        {
+            // Arrange
+            var anonObject = new { Number = 3 };
+
+            // Act
+            var result = ObjectExtensions.GetValueFromAnonymousTypeOrDefault<int>(anonObject, property);
+
+            // Assert            
+            result.Should().Be(default(int));
+        }
+
         #endregion Get Value From Anon Tests
+
+        #region Is Property In Anonymous Type
+
+        [Fact]
+        public void test_is_property_in_anonymous_type_with_null_object_returns_false()
+        {
+            // Arrange
+            ExpandoObject anonObject = null;
+
+            // Act
+            var result = ObjectExtensions.IsPropertyInAnonymousType(anonObject, "Number");
+
+            // Assert
+            result.Should().BeFalse();
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("    ")]
+        [InlineData(null)]
+        public void test_is_property_in_anonymous_type_with_null_or_empty_property_returns_false(string property)
+        {
+            // Arrange
+            var anonObject = new { Number = 3 };
+
+            // Act
+            var result = ObjectExtensions.IsPropertyInAnonymousType(anonObject, property);
+
+            // Assert
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void test_is_property_in_anonymous_type_with_unknown_property_returns_false()
+        {
+            // Arrange
+            var anonObject = new { Number = 3 };
+
+            // Act
+            var result = ObjectExtensions.IsPropertyInAnonymousType(anonObject, "Something");
+
+            // Assert
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void test_is_property_in_anonymous_type_with_known_property_returns_true()
+        {
+            // Arrange
+            var anonObject = new { Number = 3 };
+
+            // Act
+            bool result = ObjectExtensions.IsPropertyInAnonymousType(anonObject, "Number");
+
+            // Assert
+            result.Should().BeTrue();
+        }
+        #endregion
 
         #region Object Clone Tests
 
--- Useful.Extensions/ObjectExtensions.cs
+++ Useful.Extensions/ObjectExtensions.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Useful.Extensions
@@ -13,13 +12,70 @@ namespace Useful.Extensions
         /// <summary>
         /// Extract the value from a anonymous object by property name
         /// </summary>
-        /// <param name="src"></param>
+        /// <param name="src">The anonymous object</param>
         /// <param name="property">The name of the property to get the value from</param>
         /// <returns>The value of the given property</returns>
         public static T GetValueFromAnonymousType<T>(object src, string property)
         {
+            if (src == null)
+            {
+                throw new ArgumentNullException(nameof(src));
+            }
+
+            if (string.IsNullOrWhiteSpace(property))
+            {
+                throw new ArgumentNullException(nameof(property));
+            }
+
             var type = src.GetType();
-            return (T)type.GetProperty(property).GetValue(src, null);
+            var objectProperty = type.GetProperty(property);
+
+            if (objectProperty == null)
+            {
+               throw new ArgumentException("Property not found in anonymous object");
+            }
+            return (T)objectProperty.GetValue(src, null);
+        }
+
+        /// <summary>
+        /// Extract the value from an anonymouse object or return the default if not found
+        /// </summary>
+        /// <param name="src">The anonymous object</param>
+        /// <param name="property">The name of the property to get the value from</param>
+        /// <returns>The value of the given property or the default</returns>
+        public static T GetValueFromAnonymousTypeOrDefault<T>(object src, string property)
+        {
+            try
+            {
+                return GetValueFromAnonymousType<T>(src, property);
+            }
+            catch (ArgumentException)
+            {
+                return default(T);
+            }          
+        }
+
+        /// <summary>
+        /// Check if an anonymous object has a property with a given name
+        /// </summary>
+        /// <param name="src">The anonymous object</param>
+        /// <param name="property">The name of the property to get the value from</param>
+        /// <returns>A boolean denoting if the property exists</returns>
+        public static bool IsPropertyInAnonymousType(object src, string property)
+        {
+            if (src == null || string.IsNullOrWhiteSpace(property))
+            {
+                return false;
+            }
+
+            var type = src.GetType();
+            var objectProperty = type.GetProperty(property);
+            if (objectProperty == null)
+            {
+                return false;
+            }
+
+            return true;
         }
 
         /// <summary>
