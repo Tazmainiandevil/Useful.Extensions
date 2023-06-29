ac103e23bc4a0f331d49b84b599fb33b99c21f26
Mark Johnson <mark@twistersdomain.co.uk>
2023-06-29 08:14:08 +0100

    Add Get Description for enums

--- src/Useful.Extensions/GlobalUsings.cs
+++ src/Useful.Extensions/GlobalUsings.cs
@@ -1,5 +1,6 @@
 ﻿global using System;
 global using System.Collections.Generic;
+global using System.ComponentModel;
 global using System.Globalization;
 global using System.Linq;
 global using System.Reflection;
--- tests/Useful.Extensions.Tests/GlobalUsings.cs
+++ tests/Useful.Extensions.Tests/GlobalUsings.cs
@@ -1,6 +1,7 @@
 ﻿global using FluentAssertions;
 global using System;
 global using System.Collections.Generic;
+global using System.ComponentModel;
 global using System.Dynamic;
 global using System.Globalization;
 global using System.Linq;
